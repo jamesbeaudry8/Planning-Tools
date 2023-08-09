ed0f6d292459aaa45652791e362edf5c483366c5
James Beaudry <jamesbeaudry8@gmail.com>
2023-08-09 12:05:34 -0400

    Add fetcher scripts for windows and linux

---
+++ fetcher.ps1
@@ -0,0 +1,1 @@
+wget https://raw.githubusercontent.com/jamesbeaudry8/Planning-Tools/main/Project%20Planning.md -outfile "./Project Planning.md"
---
+++ fetcher.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+wget https://raw.githubusercontent.com/jamesbeaudry8/Planning-Tools/main/Project%20Planning.md
